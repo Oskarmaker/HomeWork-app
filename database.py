f90583403452a490fc4d3d2d3626229fe000164d
Vladimir <vova.telendiy@mail.ru>
2026-06-29 01:39:33 +0300

    Создание файла для коннекта с БД

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+DATABASE_URL = "sqlite:///./homework-app.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
