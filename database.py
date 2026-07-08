941342b598c28f75825126b948fe250cb6ad1327
Vladimir <vova.telendiy@mail.ru>
2026-07-09 00:01:05 +0300

    Добавление зависимостей и Docker

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-DATABASE_URL = "sqlite:///./homework-app.db"
+DATABASE_URL = "sqlite:///./data/homework-app.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
